00e7982a6ae8db5cd053c29af09282d576cf90be
Isaias Ponce <isaiasponce-12@hotmail.es>
2021-10-05 22:12:05 -0500

    Uploaded from my computer/Initial Stage

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Automated-Word-document-with-python
 I did this as a hobby, the idea in the future is to customize the styles selections using a GUI
+
+Automate your docx files by modifying the toDo.txt and customize the style by modifying the dictionary in functions.py
+
+# Install python docx library
+pip3 install python-docx
+
+# Run this program!
+$ python3 main.py
+
+# You can customize the styles by:
+Modify the dictionary in fuctions.py
+
+# Content is included in toDo.txt as a list of "Instructions"
+
+/ti this is a title. /
+/li this is a literal. /
+/sb this is a subtitle. /
+/ch this a image title. / 
+/no this is a paragraph. /
+
+
+
---
+++ main.py
@@ -0,0 +1,40 @@
+import docx
+from functions import *
+import os
+import re
+from IOFunctions import *
+todo_filename='toDo.txt'
+filename='labs.docx'
+
+
+def images_section(tuple,grupo,doc,filename):
+    for key in grupo:
+        value=tuple[int(key)-1][1]
+
+        imagename=str(key)+" "+str(value)+".png"
+
+
+        add_image(doc, imagename)
+        write(doc, "ch", str(value), filename)
+
+def hacer_word(filename,todo_filename):
+    lista = os.listdir()
+    tupla_imagenes = image_tuple_generator(lista)
+    doc = docx.Document(filename)
+    instrucciones = file2list(todo_filename)
+
+    for instruccion in instrucciones:
+        pattern = r"\w*"
+        match = re.search(pattern, instruccion)
+        if match[0] == "im":  # La primera palabra
+            pattern2 = r"[0-9]+"
+            match = re.findall(pattern2, instruccion)
+            images_section(tupla_imagenes, match, doc,filename)  # enviar las imagenes a subir jeje
+        else:
+
+            write(doc, match[0], instruccion[3:], filename)
+
+    doc.save(filename)
+    print("HECHO! ")
+
+hacer_word(filename, todo_filename)
